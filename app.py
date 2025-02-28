49d774602a0d3748b6c5988ee233d8167c1d82b0
altafalinadaf <altafalinadaf@gmail.com>
2025-02-28 16:36:22 +0530

    adding products link

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "hello world"
 
+@app.route('/products')
+def products():
+    return "this is products page "
+
 if __name__=="__main__":
     app.run()
